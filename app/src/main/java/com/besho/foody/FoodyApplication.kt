138ae5bf7fd1735286767dd2f05148c31a911617
beshoybartous <50753157+beshoybartous@users.noreply.github.com>
2023-06-24 15:09:56 +0300

    init commit

---
+++ app/src/main/java/com/besho/foody/FoodyApplication.kt
@@ -0,0 +1,8 @@
+package com.besho.foody
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FoodyApplication: Application() {
+}
---
+++ app/src/main/java/com/besho/foody/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.besho.foody
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.besho.foody.ui.theme.FoodyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FoodyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FoodyTheme {
+        Greeting("Android")
+    }
+}
